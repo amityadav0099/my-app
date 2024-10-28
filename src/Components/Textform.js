816101c0cdfbcdc7108eacaf58f223d51e006b48
amityadav0099 <yadavamit5675@gmail.com>
2024-10-28 16:24:37 +0530

    optimized

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 
 
 export default function Textform(props) {
+
   const[text, setText] = useState('');
 
     const handleupClick=()=>{
@@ -31,6 +32,7 @@ const handleCopy=()=>{
   var text = document.getElementById("myBox");
   text.select();
   navigator.clipboard.writeText(text.value);
+  document.getSelection().removeAllRanges();
   props.showAlert("Copied to clipboard!", "success");
 };
 const handleExtraSpaces=()=>{
@@ -44,26 +46,27 @@ const handleExtraSpaces=()=>{
   return (
     <>
     <div className="container" style={{color : props.mode==='dark'?'white':'#042743'}}>
-    <h1 >{props.heading}</h1>
+    <h1 className='mb-2'>{props.heading}</h1>
 <div className="mb-3">
 {/* <label for="myBox" className="form-label">Example textarea</label> */}
-<textarea className="form-control" value= {text} onChange={handleonChange} style={{backgroundColor : props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'white':'#042743'}}id="myBox" rows="8">
+<textarea className="form-control" value= {text} onChange={handleonChange}
+ style={{backgroundColor : props.mode==='dark'?'#13466e':'white', color:props.mode==='dark'?'white':'#042743'}}id="myBox" rows="8">
   
 </textarea>
 </div>
-<button className="btn btn-primary mx-1" onClick={handleupClick} >{props.buttonname}</button>
-<button className="btn btn-primary mx-1" onClick={handleLoClick} >{props.buttonname2}</button>
-<button className="btn btn-primary mx-1" onClick={handleclearClick} >{props.buttonname3}</button>
-<button className="btn btn-primary mx-1 my-1" onClick={handleCopy} >Copy Text</button>
-<button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces} >Remove Extra spaces</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleupClick} >{props.buttonname}</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleclearClick} >{props.buttonname3}</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick} >{props.buttonname2}</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy} >Copy Text</button>
+<button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces} >Remove Extra spaces</button>
 
 <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}} >
   <h1>Your Text summary</h1>
-  {/* <p>{text.split(" ").length} words, {text.length}characters</p> */}
-  <p>{text.split(" ").filter(word => word.trim() !== "").length}: words, {text.length} Characters</p>
-  <p>{0.008*text.split(" ").length} minutes takes to read</p>
+  <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words, {text.length}characters</p>
+  {/* <p>{text.split(" ").filter(word => word.trim() !== "").length}: words, {text.length} Characters</p> */}
+  <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} minutes takes to read</p>
   <h2>preview</h2>
-  <p>{text.length>0?text:"Enter something in the above textbox to preview here"}</p>
+  <p>{text.length>0?text:"Nothing to preview!"}</p>
   </div>
 </div>
     </>
